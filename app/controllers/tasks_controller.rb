a120213fe7a219e72048990e940ba2e77f5a4598
Peggy Griffin <seishingithub@gmail.com>
2014-07-18 23:35:54 -0600

    Refactored TasksController to remove repetition via 'before' action and private params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
+  before_action :get_task_list, only: [:new, :create]
 
   def new
     @task = Task.new
-    @task_list = TaskList.find(params[:task_list_id])
   end
 
   def create
-    @task_list = TaskList.find(params[:task_list_id])
     @task = Task.create(task_params)
     if @task.save
       redirect_to root_path, notice: "Task was created successfully!"
@@ -20,6 +19,10 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:description, :due_date).merge(task_list_id: params[:task_list_id])
+  end
+
+  def get_task_list
+    @task_list = TaskList.find(params[:task_list_id])
   end
 
 end
